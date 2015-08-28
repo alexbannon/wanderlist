102e82cd13aa8970e08816be576e4431128f16fd
Christine Lovett <clovett07@gmail.com>
2015-08-28 10:46:40 -0400

    heroku

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ var Connection = require("./db/connection");
 var User = Connection.models.User;
 var userId;
 var pg = require('pg');
-// var database = "";
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 
 app.use(bodyParser.json());
