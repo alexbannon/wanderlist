6c0f99b977934a5039a912b44078edabcb6ffb1f
Christine Lovett <clovett07@gmail.com>
2015-08-28 11:04:35 -0400

    trying

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,16 @@
 var Sequelize = require("sequelize");
-
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    logging:  true //false
-  })
-} else {
-  // the application is executed on the local machine
-  sequelize = new Sequelize("postgres:///wanderlist");
-}
+var db_connection = new Sequelize(process.env.DATABASE_URL || "postgres:///wanderlist");
+// if (process.env.DATABASE_URL) {
+//   // the application is executed on Heroku ... use the postgres database
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect:  'postgres',
+//     protocol: 'postgres',
+//     logging:  true //false
+//   })
+// } else {
+//   // the application is executed on the local machine
+//   sequelize = new Sequelize("postgres:///wanderlist");
+// }
 
 var User = sequelize.import("../models/user");
 var Pin = sequelize.import("../models/pin");
